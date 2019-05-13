8a23d3f6cd07cb2f3669635a63a145dd181de6e4
Keith Isham <kisham8899@gmail.com>
2019-05-13 16:46:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,12 +22,13 @@ def initialize(name, artist, genre)
   end
 
 
-  def self.artist
-    @@artists.uniq
-  end
-  def genre
-@@genres.uniq
-  end
+  def self.artists
+     @@artists.uniq
+   end
+ 
+   def self.genres
+     @@genres.uniq
+   end
 
   def self.genre_count
     genre_count = Hash.new(0)
