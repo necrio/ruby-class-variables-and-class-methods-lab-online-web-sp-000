cc47be214aea6ab6cb761d685718880eb9fdf2ad
Keith Isham <kisham8899@gmail.com>
2019-05-13 16:20:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,9 @@ class Song
 @@count = 0
 @@genre = []
 
-def initialize
-    @@count += 1
-  end
+# def initialize
+#     @@count += 1
+#   end
 
   def self.genre_count
     @@genre.inject(Hash.new(0)) { |total, i| total[i] += 1; total}
